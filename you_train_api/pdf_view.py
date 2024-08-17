84860f63a54ca1c4282a8f746d0e483d0ec75716
korsem <korneliasem@gmail.com>
2024-08-17 08:46:53 +0200

    fixed some views

--- you_train_api/pdf_view.py
+++ you_train_api/pdf_view.py
@@ -1,17 +1,20 @@
 import io
 import os
+
 from django.http import HttpResponse
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import fonts
+from reportlab.platypus import Table, TableStyle
 from reportlab.lib.units import cm
 from reportlab.pdfgen import canvas
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak
 from reportlab.lib.enums import TA_RIGHT, TA_CENTER
 from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib import colors
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.shortcuts import render, redirect, get_object_or_404
 
 from you_train_api.calendar_methods import get_polish_day_of_week
@@ -88,7 +91,7 @@ class BaseTrainingPlanPDF:
                 Paragraph(f"Cele: {self.training_plan.goal}", self.styles["normal"])
             )
 
-        self.content.append(Spacer(1, 12))
+        self.content.append(Spacer(1, 8))
 
     def add_footer(self, canvas, doc):
         canvas.saveState()
@@ -119,49 +122,165 @@ class BaseTrainingPlanPDF:
 
 
 class CyclicTrainingPlanPDF(BaseTrainingPlanPDF):
-    # dodaj do tego co aktualnie jest w header plan.start_date i plan.end_date jako start_date + relativetimedelta(weeks=cycle_length)
-    # i informacje na ile tygodni jest plan tzn cycle_length
+
+    def add_header(self):
+        super().add_header()
+
+        start_date = self.training_plan.workout_plan.start_date
+        cycle_length = self.training_plan.workout_plan.cycle_length
+        end_date = start_date + timedelta(weeks=cycle_length)
+
+        self.content.append(
+            Paragraph(
+                f'Plan ważny od: {start_date.strftime("%d-%m-%Y")} do: {end_date.strftime("%d-%m-%Y")}',
+                self.styles["normal"],
+            )
+        )
+        self.content.append(
+            Paragraph(
+                f"Ilość tygodni trwania planu: {cycle_length}", self.styles["normal"]
+            )
+        )
+        self.content.append(Spacer(1, 12))
+
     def add_workouts(self):
+        # tworzenie tabeli 2x7 z dniami tygodnia i odpowiadającymi im treningami
+        days_of_week = [get_polish_day_of_week(i) for i in range(7)]
+        workouts_in_week = [""] * 7
+
         workouts = WorkoutInPlan.objects.filter(
             workout_plan__training_plan=self.training_plan
-        ).order_by(
-            "day_of_week"
-        )  # powinny być w tabelce
+        ).order_by("day_of_week")
+        for workout_in_plan in workouts:
+            day_index = workout_in_plan.day_of_week
+            title = workout_in_plan.workout.title
+            workouts_in_week[day_index] = Paragraph(title, self.styles["normal"])
+
+        data = [days_of_week, workouts_in_week]
+
+        available_width = A4[0] - 3 * cm
+        col_widths = [available_width * 0.15] * 7
+
+        table = Table(data, colWidths=col_widths)
+        table.setStyle(
+            TableStyle(
+                [
+                    ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
+                    ("TEXTCOLOR", (0, 0), (-1, -1), colors.black),
+                    ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+                    ("FONTNAME", (0, 0), (-1, 0), "DejaVuSans-Bold"),
+                    ("FONTSIZE", (0, 0), (-1, -1), 9),
+                    ("BOTTOMPADDING", (0, 0), (-1, -1), 18),
+                    ("BACKGROUND", (0, 1), (-1, -1), colors.whitesmoke),
+                    ("GRID", (0, 0), (-1, -1), 1, colors.black),
+                    ("WORDWRAP", (0, 1), (-1, -1), True),
+                ]
+            )
+        )
+
+        self.content.append(table)
+        self.content.append(Spacer(1, 16))
+
+        self.content.append(
+            Paragraph("szczegóły treningów:", self.styles["Bold"])
+        )  # tu powinien być większa czcionka
+
+        # szczegółowe info
         for workout_in_plan in workouts:
             workout = workout_in_plan.workout
-            self.content.append(
-                Paragraph(f"Workout: {workout.title}", self.styles["Bold"])
-            )
+
             self.content.append(
                 Paragraph(
-                    f"Date: {get_polish_day_of_week(workout_in_plan.day_of_week)}",
-                    self.styles["normal"],
+                    f"Workout: {workout.title} - {get_polish_day_of_week(workout_in_plan.day_of_week)}",
+                    self.styles["Bold"],
                 )
             )
-            for segment in workout.segments.all():
-                if segment.reps:
+
+            if (segment_numb := len(workout.segments.all())) > 1:
+                # Informacje o segmentach treningu
+                for numb, segment in enumerate(workout.segments.all()):
+                    self.content.append(Spacer(1, 5))
+
                     self.content.append(
                         Paragraph(
-                            f"Segment Reps: {segment.reps}", self.styles["normal"]
+                            f"Blok treningowy {numb + 1} / {segment_numb} do wykonania {segment.reps} razy",
+                            self.styles["normal"],
                         )
                     )
-                if segment.rest_time:
+                    if segment.rest_time:
+                        self.content.append(
+                            Paragraph(
+                                f"na {segment.rest_time} przerwy", self.styles["normal"]
+                            )
+                        )
+
+                    # Informacje o ćwiczeniach w segmencie
+                    for exercise in segment.exercises.all():
+                        self.content.append(
+                            Spacer(1, 6)
+                        )  # Dodanie mniejszego odstępu przed ćwiczeniem
+
+                        # Nazwa ćwiczenia z dodatkowym wcięciem
+                        self.content.append(
+                            Paragraph(
+                                f"Exercise: {exercise.name}", self.styles["normal"]
+                            )
+                        )
+            else:
+                # jeśli jest tylko jeden segment - to nie wyśweitlaj info o segmencie
+                segment = workout.segments.all().first()
+                if (reps := segment.reps) > 1:
                     self.content.append(
                         Paragraph(
-                            f"Rest Time: {segment.rest_time}", self.styles["normal"]
+                            f"Całość do wykonania {reps} razy", self.styles["normal"]
                         )
                     )
-                self.content.append(Spacer(1, 12))
-                for exercise in segment.exercises.all():
+                for exercise in segment.exerciseinsegment_set.all():
+                    self.content.append(Spacer(1, 6))
+
+                    # jak wykonywać dane ćwiczenie
                     self.content.append(
-                        Paragraph(f"Exercise: {exercise.name}", self.styles["Bold"])
+                        Paragraph(
+                            f"Exercise: {exercise.exercise.name}", self.styles["normal"]
+                        )
                     )
-                    self.content.append(Spacer(1, 12))
+                    if exercise.reps:
+                        self.content.append(
+                            Paragraph(
+                                f"Powtórzenia: {exercise.reps}", self.styles["normal"]
+                            )
+                        )
+                    if exercise.duration:
+                        self.content.append(
+                            Paragraph(
+                                f"Czas: {exercise.duration}", self.styles["normal"]
+                            )
+                        )
+                    self.content.append(
+                        Paragraph(
+                            f"Przerwa: {exercise.rest_time}", self.styles["normal"]
+                        )
+                    )
+
+            # odstęp po każdyum treningu
             self.content.append(Spacer(1, 12))
 
 
 class NonCyclicTrainingPlanPDF(BaseTrainingPlanPDF):
-    # dodaj do tego co aktualnie jest w header plan.start_date i plan.end_date jako start_date
+    def add_header(self):
+        super().add_header()
+
+        start_date = self.training_plan.workout_plan.start_date
+        end_date = self.training_plan.workout_plan.end_date
+
+        self.content.append(
+            Paragraph(
+                f'Plan ważny od: {start_date.strftime("%d-%m-%Y")} do: {end_date.strftime("%d-%m-%Y")}',
+                self.styles["normal"],
+            )
+        )
+        self.content.append(Spacer(1, 12))
+
     def add_workouts(self):
         workouts = WorkoutInPlan.objects.filter(
             workout_plan__training_plan=self.training_plan
@@ -188,15 +307,33 @@ class NonCyclicTrainingPlanPDF(BaseTrainingPlanPDF):
                         )
                     )
                 self.content.append(Spacer(1, 12))
-                for (
-                    exercise
-                ) in (
-                    segment.exercises.all()
-                ):  # exercise in plan żeby wyświuetlić powtórzenia lub duration i przerwe?
+
+                for exercise in segment.exerciseinsegment_set.all():
+                    self.content.append(Spacer(1, 6))
+
                     self.content.append(
-                        Paragraph(f"Exercise: {exercise.name}", self.styles["Bold"])
+                        Paragraph(
+                            f"Exercise: {exercise.exercise.name}", self.styles["normal"]
+                        )
                     )
-                    self.content.append(Spacer(1, 12))
+                    if exercise.reps:
+                        self.content.append(
+                            Paragraph(
+                                f"Powtórzenia: {exercise.reps}", self.styles["normal"]
+                            )
+                        )
+                    if exercise.duration:
+                        self.content.append(
+                            Paragraph(
+                                f"Czas: {exercise.duration}", self.styles["normal"]
+                            )
+                        )
+                    self.content.append(
+                        Paragraph(
+                            f"Przerwa: {exercise.rest_time}", self.styles["normal"]
+                        )
+                    )
+
             self.content.append(Spacer(1, 12))
 
 
